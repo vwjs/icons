9fc8c48f152be33fb4904fd869c5d1e4e3dba97d
kalanakt <e19198@eng.pdn.ac.lk>
2022-12-07 10:37:55 +0530

    Added Icons
    - Collage Icon
    - Dashboard Icon
    - Draft Icon
    - InkBottle Icon
    - ReplyAll Icon
    - Reply Icon
    - Usb Icon
    
    Co-Authored-By: kalana kithmina <e19198@eng.pdn.ac.lk>
    Co-Authored-By: MiiNiiX <96678838+miiniix@users.noreply.github.com>

---
+++ src/ReplyIcon.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { SVGProps, Ref, forwardRef } from 'react'
+
+const ReplyIcon = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
+  <svg xmlns='http://www.w3.org/2000/svg' width={24} height={24} ref={ref} {...props}>
+    <path fill='none' d='M0 0h24v24H0z' />
+    <path d='M11 20 1 12l10-8v5c5.523 0 10 4.477 10 10 0 .273-.01.543-.032.81a9.003 9.003 0 0 0-7.655-4.805L13 15h-2v5zm-2-7h4.034l.347.007c1.285.043 2.524.31 3.676.766A7.984 7.984 0 0 0 11 11H9V8.161L4.202 12 9 15.839V13z' />
+  </svg>
+)
+
+const ForwardRef = forwardRef(ReplyIcon)
+export default ForwardRef
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+import CollageIcon from './CollageIcon'
+import DashboardIcon from './DashboardIcon'
+import DraftIcon from './DraftIcon'
 import HomeIcon from './HomeIcon'
+import InkBottleIcon from './InkBottleIcon'
+import ReplyAllIcon from './ReplyAllIcon'
+import ReplyIcon from './ReplyIcon'
+import UsbIcon from './UsbIcon'
 
-export { HomeIcon }
+export { HomeIcon, ReplyIcon, ReplyAllIcon, InkBottleIcon, CollageIcon, DashboardIcon, UsbIcon, DraftIcon }
