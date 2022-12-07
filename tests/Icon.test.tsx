ae028a4e5d79b96921cea781ab5e9b677ab12a74
kalanakt <e19198@eng.pdn.ac.lk>
2022-12-07 19:29:56 +0530

    update jest module testing && pause test in docs site
    Co-Authored-By: kalana kithmina <e19198@eng.pdn.ac.lk>
    Co-Authored-By: MiiNiiX <96678838+miiniix@users.noreply.github.com>

---
+++ tests/Icon.test.tsx
@@ -0,0 +1,211 @@
+import * as React from 'react'
+import { render } from '@testing-library/react'
+import 'jest-canvas-mock'
+
+import {
+  HomeIcon,
+  ReplyIcon,
+  ReplyAllIcon,
+  InkBottleIcon,
+  CollageIcon,
+  DashboardIcon,
+  UsbIcon,
+  DraftIcon,
+  AncientGateIcon,
+  AncientPavilionIcon,
+  BankIcon,
+  BuildingIcon,
+  BuildingiIcon,
+  BuildingiiIcon,
+  BuildingiiiIcon,
+  BuildingivIcon,
+  GovernmentIcon,
+  HomeiIcon,
+  HomeiiIcon,
+  HomeiiiIcon,
+  HomeivIcon,
+  HomevIcon,
+  HomeviIcon,
+  HomeviiIcon,
+  HomeviiiIcon,
+  HomeixIcon,
+  HomexIcon,
+  HomexiIcon,
+  HomexiiIcon,
+  HospitalIcon,
+  HotelIcon,
+  StoreIcon,
+  StoreiIcon,
+  StoreiiIcon,
+} from '../src'
+
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<HomeIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<ReplyIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<ReplyAllIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<InkBottleIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<CollageIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<DashboardIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<UsbIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<DraftIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<AncientGateIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<AncientPavilionIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<BankIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<BuildingIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<BuildingiIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<BuildingiiIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<BuildingiiiIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<BuildingivIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<StoreiiIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<StoreiIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<StoreIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<HotelIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<HospitalIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<GovernmentIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<HomeiIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<HomeiiIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<HomeiiiIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<HomeivIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<HomevIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<HomeviIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<HomeviiIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<HomeviiiIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<HomeixIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<HomexIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<HomexiIcon />)
+  })
+})
+describe('Common render', () => {
+  it('renders without crashing', () => {
+    render(<HomexiiIcon />)
+  })
+})
