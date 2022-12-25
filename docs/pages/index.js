0dc14e6d8a5b9f26fe79156171cae3974fa3df95
kalanakt <e19198@eng.pdn.ac.lk>
2022-12-25 19:20:07 +0530

    Update index.js

--- docs/pages/index.js
+++ docs/pages/index.js
@@ -3,8 +3,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { HomeIcon } from '@vwjs/icons'
+import { useState } from 'react'
 
 export default function Home() {
+  const [checked, setChecked] = useState(false)
   return (
     <div className={styles.container}>
       <Head>
@@ -14,41 +16,9 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <HomeIcon />
-        <h1 className={styles.title}>
-          Welcome to <a href='https://nextjs.org'>Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href='https://nextjs.org/docs' className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href='https://nextjs.org/learn' className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href='https://github.com/vercel/next.js/tree/canary/examples' className={styles.card}>
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href='https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            target='_blank'
-            rel='noopener noreferrer'
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
+      <div className="bg-red-900">
+        <HomeIcon onClick={() => setChecked(!checked)}/>
+      </div>
       </main>
 
       <footer className={styles.footer}>
